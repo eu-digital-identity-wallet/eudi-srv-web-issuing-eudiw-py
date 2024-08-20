b6d3c665867a64044e8c55d138e2531ee5a7f53f
LuisPereira23 <LuismPereira23@gmail.com>
2024-08-20 14:08:19 +0100

    update dockerfile. Initial work on credentials for potential hackaton

--- Dockerfile
+++ Dockerfile
@@ -17,9 +17,19 @@ WORKDIR /app
 
 COPY app/requirements.txt /app/
 
+RUN mkdir -p /etc/eudiw/pid-issuer/cert/
+
+RUN mkdir -p /etc/eudiw/pid-issuer/privkey/
+
+COPY app/private/certs/ /etc/eudiw/pid-issuer/cert/
+
+COPY app/private/privkeys/ /etc/eudiw/pid-issuer/privkey/
+
 RUN pip3 install --no-cache-dir -r requirements.txt
 
 COPY . /app
+
+ENV REQUESTS_CA_BUNDLE=app/cert.pem
 
 EXPOSE 5000
 
@@ -27,4 +37,4 @@ ENV FLASK_APP=app
 ENV FLASK_RUN_PORT=5000
 ENV FLASK_RUN_HOST=0.0.0.0
 
-CMD ["flask", "run"]
+CMD ["flask", "run", "--cert=app/cert.pem", "--key=app/key.pem"]
--- app/app_config/config_service.py
+++ app/app_config/config_service.py
@@ -271,6 +271,41 @@ class ConfService:
             "validity": qeaa_validity,
             "organization_name": "Test QEAA issuer",
             "namespace": "org.iso.23220.2.photoid.1",
+        },
+        "eu.europa.ec.eudi.por.1": {
+            "issuing_authority": "Test QEAA issuer",
+            "organization_id": pid_organization_id,
+            "validity": qeaa_validity,
+            "organization_name": "Test QEAA issuer",
+            "namespace": "eu.europa.ec.eudi.por.1",
+        },
+        "eu.europa.ec.eudi.iban.1": {
+            "issuing_authority": "Test QEAA issuer",
+            "organization_id": pid_organization_id,
+            "validity": qeaa_validity,
+            "organization_name": "Test QEAA issuer",
+            "namespace": "eu.europa.ec.eudi.por.1",
+        },
+        "eu.europa.ec.eudi.hiid.1": {
+            "issuing_authority": "Test QEAA issuer",
+            "organization_id": pid_organization_id,
+            "validity": qeaa_validity,
+            "organization_name": "Test QEAA issuer",
+            "namespace": "eu.europa.ec.eudi.hiid.1",
+        },
+        "eu.europa.ec.eudi.tax.1": {
+            "issuing_authority": "Test QEAA issuer",
+            "organization_id": pid_organization_id,
+            "validity": qeaa_validity,
+            "organization_name": "Test QEAA issuer",
+            "namespace": "eu.europa.ec.eudi.tax.1",
+        },
+        "eu.europa.ec.eudi.msisdn.1": {
+            "issuing_authority": "Test QEAA issuer",
+            "organization_id": pid_organization_id,
+            "validity": qeaa_validity,
+            "organization_name": "Test QEAA issuer",
+            "namespace": "eu.europa.ec.eudi.msisdn.1",
         }
         
     }
@@ -289,6 +324,11 @@ class ConfService:
             "eu.europa.ec.eudi.pseudonym_over18_mdoc",
             "eu.europa.ec.eudi.pseudonym_over18_mdoc_deferred_endpoint",
             "eu.europa.ec.eudi.photoid",
+            "eu.europa.ec.eudi.por_mdoc",
+            "eu.europa.ec.eudi.iban_mdoc",
+            "eu.europa.ec.eudi.hiid_mdoc",
+            "eu.europa.ec.eudi.tax_mdoc",
+            "eu.europa.ec.eudi.msisdn_mdoc",
         ],
     }
 
