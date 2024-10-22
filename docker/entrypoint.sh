4f440ad24c9e534e15673ecbab8ca5e53d1ba231
haeser <florian.haeser@mohemian.com>
2024-10-22 10:10:16 +0200

    Copy secret from mount to app config

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,9 @@ if [ ! -f "$SECRETS_CONFIG_DIR/config_secrets.py" ]; then
     echo "Error: config_secrets.py not found in $SECRETS_CONFIG_DIR. Exiting."
     exit 1
 fi
+
+cp "$SECRETS_CONFIG_DIR/config_secrets.py" /home/flaskuser/eudi-srv-web-issuing-eudiw-py/app/app_config/
+
 FLASK_RUN_CMD="./venv/bin/flask run"
 
 if [ -f "$SECRETS_CONFIG_DIR/cert.pem" ] && [ -f "$SECRETS_CONFIG_DIR/key.pem" ]; then
